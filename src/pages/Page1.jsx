c6f117a5fd1206c4ab530d616b9158ab67314532
mañana <hernandezrichard647@gmail.com>
2024-04-16 13:01:13 +0200

    holas

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react';
 
-const Page2 = () => (
-        <div>    
-                <h1>Page2</h1>
-        </div>
-)
+const Page2 = () => {
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character ')
+            .then(res => res.json())
+            .then(res => setNames(res.results));
+    }, []);
+    console.log(names);
+    return (
+        <>
+            {names.map((character) => (
+                <div key={character.id}>
+                    <p>{character.name}</p>
+                    <img src={character.image} alt={character.name} />
+                    <p>{character.species}</p>
+                    <br />
+                    <br />
+                    <br />
+                </div>
+            ))}
+        </>
+    );
+};
 
 export default Page2;
